59fd54c166e6eebe4045d6b0b016d8cf8df3e663
vlad <koooornienko@gmail.com>
2026-07-28 21:38:02 +0500

    fix

--- src/billings/billing.module.ts
+++ src/billings/billing.module.ts
@@ -3,11 +3,12 @@ import { BillingController } from './billing.controller';
 import { PrismaService } from '../prisma/prisma.service'; // Убедись, что путь правильный
 import { RobokassaService } from './robokassa.service';
 import { ConfigModule } from '@nestjs/config';
+import { LavaService } from './lava.service';
 
 @Module({
   imports: [ConfigModule],
   controllers: [BillingController],
-  providers: [RobokassaService, PrismaService],
+  providers: [RobokassaService, PrismaService, LavaService],
   exports: [RobokassaService], // Экспортируем, если понадобится в других модулях
 })
 export class BillingModule {}
