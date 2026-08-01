981993cb600e14f9fa47d1cbf027a70e51136e05
vlad <koooornienko@gmail.com>
2026-08-02 01:39:15 +0500

    temp

--- src/billings/lava.service.ts
+++ src/billings/lava.service.ts
@@ -19,7 +19,7 @@ export class LavaService {
     amount: number,
     currency: string,
   ) {
-    const apiKey = this.config.get('LAVA_API_KEY');
+    const apiKey = this.config.get('LAVA_SECRET_KEY');
 
     const requestData = {
       email: email,
